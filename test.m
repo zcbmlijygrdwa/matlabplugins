35bd95710ebe7fe41a2234aaae2369bdb272edea
Zhenyu Yang <zhenyuyang@ucsb.edu>
2019-02-08 05:47:01 -0800

    added projetion test

--- test.m
+++ test.m
@@ -1,47 +1,53 @@
 clear all;
-close all;
+% close all;
+
+cam_t = [-4,5,5]'
+cam_r = [2.7,0.15,-1.7]
+
+rot = rotmat3d(cam_r(1),cam_r(2),cam_r(3))
+t = cam_t
 
 
-RSI0_out = [];
-RSI1_out = [];
-dataHis = [];
-RSI0_state.period = 10;
-RSI0_state.initialized = false;
+K = camProjMat(609,609,305,225)
 
-RSI1_state(1) = 0; %RSI1_state.initialized = false;
-RSI1_state(2) = 10; %RSI1_state.period = 200;
+points3d = [1,0,0;
+    0,-1,0;
+    -1,0,0;
+    0,2,0;
+    0,5,0;];
 
-newData = 0;
-for i =1:10000
-    
-    newData = rand();
-    
-    dataHis = [dataHis;newData];
+points3d = [-0.33,0.1,0;
+    0.33,0.1,0;
+    0.33,0.6,0;
+    -0.33,1.2,0];
+
+numOfP = size(points3d,1);
+
+points3d = [points3d,ones(size(points3d,1),1)]'
+
+trans = [rot,t];
+
+ trans = [trans;zeros(1,4)];
+ trans(4,4) = 1
+trans = inv(trans);
+trans = trans(1:3,:);
+
+new_points3d = trans*points3d;
+
+points2d = K*(new_points3d)
+
+img = zeros(450,610);
+
+for i = 1:size(points2d,2)
+    points2d(:,i) = points2d(:,i)/points2d(3,i);
+    tx = int32(points2d(1,i));
+    ty = int32(points2d(2,i));
+    if(tx>0&&ty>0&&tx<=610&&ty<=450)
+       img(ty,tx) = 1; 
+    end
 end
 
-tic
-for i =1:10000
-    RSI0_state = RSI(RSI0_state,dataHis(i));
-    RSI0_out = [RSI0_out;RSI0_state.resultRaw];
-    
-    
-    
-end
-toc
 
-tic
-for i =1:10000
-    RSI1_state = RSI_matrix(RSI1_state,dataHis(i));
-    RSI1_out = [RSI1_out;RSI1_state(11)];
-end
-toc
+imshow(img)
 
-
-subplot(3,1,1)
-plot(dataHis,'k');
-subplot(3,1,2)
-plot(RSI0_out);
-plot(RSI1_out);
-
-sum(abs(RSI0_out-RSI1_out))
 
